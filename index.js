069b620db169cbc1e77db0b264bae080a646bedf
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-01-28 20:55:26 +0200

    test authorization

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json()); //express shoud read json response
+
 app.get("/", (req, res) => {
   res.send("Hello world wow ");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {
