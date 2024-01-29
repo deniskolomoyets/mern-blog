cbdbfb12bed2435c85a87e4538aeaff4517e2915
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-01-29 07:02:23 +0200

    create model UserModel

--- index.js
+++ index.js
@@ -13,26 +13,7 @@ const app = express();
 
 app.use(express.json()); //express shoud read json response
 
-app.get("/", (req, res) => {
-  res.send("Hello world wow ");
-});
-
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Sara Konor",
-    },
-    "secret123"
-  ); //encrypt object (with a special key )
-
-  res.json({
-    success: true,
-    token,
-  });
-});
+app.post("/auth/register", (req, res) => {});
 
 app.listen(4444, (err) => {
   if (err) {
