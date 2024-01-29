1156405c9cac134fcb3683f575cc94d3a318dd1f
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-01-29 06:43:18 +0200

    connect to the database MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://kolomoetc:wwwwww@cluster0.muaios8.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
