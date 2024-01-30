bfd1412af634ef0c68c35c27fc4d48cf17e1795f
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-01-30 06:31:05 +0200

    create a user in mongodb, and encrypt the password(bcrypt lib)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
@@ -16,14 +19,26 @@ const app = express();
 
 app.use(express.json()); //express shoud read json response
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
-  res.json({
-    success: true,
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10); //password encryption algorithm
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
   });
+
+  const user = await doc.save(); //save the result that Mongodb sends (in const user)
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
