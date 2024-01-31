8cb4c120cb1b4e4e81aa272383fcbcfffb5916d1
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-01-31 06:53:38 +0200

    refactoring index.js

---
+++ controllers/UserController.js
@@ -0,0 +1,103 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+import UserModel from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10); //password encryption algorithm
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save(); //save the result that Mongodb sends (in const user)
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123", //second param, with this key I'll encrypt _id token
+      {
+        expiresIn: "30d", //how long token can live
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token }); //want to return user and token information
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to register",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Incorrect password",
+      });
+    }
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123", //second param, with this key I'll encrypt _id token
+      {
+        expiresIn: "30d", //how long token can live
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to log in",
+    });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+    res.json({ userData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "No access",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
 
-import UserModel from "./models/User.js";
 import checkAuth from "./utils/checkAuth.js";
+
+import * as UserController from "./controllers/UserController.js";
 
 mongoose
   .connect(
@@ -20,104 +19,9 @@ const app = express();
 
 app.use(express.json()); //express shoud read json response
 
-app.post("/auth/login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: "Incorrect password",
-      });
-    }
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123", //second param, with this key I'll encrypt _id token
-      {
-        expiresIn: "30d", //how long token can live
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Failed to log in",
-    });
-  }
-});
-
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10); //password encryption algorithm
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save(); //save the result that Mongodb sends (in const user)
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123", //second param, with this key I'll encrypt _id token
-      {
-        expiresIn: "30d", //how long token can live
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token }); //want to return user and token information
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Failed to register",
-    });
-  }
-});
-
-app.get("/auth/me", checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId);
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
-
-    const { passwordHash, ...userData } = user._doc;
-    res.json({ userData });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "No access",
-    });
-  }
-});
+app.post("/auth/login", UserController.login);
+app.post("/auth/register", registerValidation, UserController.register);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
   if (err) {
