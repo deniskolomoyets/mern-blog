d9f0aa4de5243a708b931599318dc4f7d557013b
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-02-02 19:41:05 +0200

    Created functionality for articles(CRUD)

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,4 +1,131 @@
-export const create = (req, res) => {
+import PostModel from "../models/Post.js";
+
+export const getAll = async (req, res) => {
   try {
-  } catch (error) {}
+    const posts = await PostModel.find()
+      .populate({ path: "user", select: ["name", "avatar"] }) // allows you to explicitly specify which fields you want to select from the associated user model, excluding passwordHash
+      .exec(); //In Mongoose, the exec() method is used to execute database queries
+    //'Connecting communications' when receiving documents. In Model I did it through 'type: mongoose.Schema.Types.ObjectId'
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Couldn't get all the articles",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id; //I'm taking out a dynamic parameter. The article id.
+
+    PostModel.findOneAndUpdate(
+      //mongoose method
+      {
+        _id: postId, //1 param - find article by id
+      },
+      {
+        $inc: { viewsCount: 1 }, //2 param - what I want update
+      },
+      {
+        returnDocument: "after", //3 param - return an updated result
+      }
+    )
+      .then((doc) => {
+        if (!doc) {
+          return res.status(404).json({
+            message: "Article not found",
+          });
+        }
+
+        res.json(doc);
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Error return article",
+          });
+        }
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error in getting selected article",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndDelete({
+      _id: postId,
+    })
+      .then((doc) => {
+        if (!doc) {
+          return res.status(404).json({
+            message: "Article not found",
+          });
+        }
+
+        res.json({ success: true });
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Failed to delete an article",
+          });
+        }
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error in getting selected article",
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.title,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId, //userId get when do an authorization check
+    });
+    const post = await doc.save();
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to create an article",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags,
+      } //what I want update
+    );
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to update an article",
+    });
+  }
 };
--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import express from "express";
 
 import mongoose from "mongoose";
 
-import { registerValidation, loginValidation } from "./validations.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 mongoose
   .connect(
@@ -22,6 +27,12 @@ app.use(express.json()); //express shoud read json response
 app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.get("/posts", PostController.getAll); //receiving all articles
+app.get("/posts/:id", PostController.getOne); //receiving one article
+app.post("/posts", checkAuth, postCreateValidation, PostController.create); //create post
+app.delete("/posts/:id", checkAuth, PostController.remove); //delete post
+app.patch("/posts/:id", checkAuth, PostController.update); //update post
 
 app.listen(4444, (err) => {
   if (err) {
