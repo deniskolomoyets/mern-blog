a2c945e0ec85790ab9d64d89992a5b321e4a6ffb
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-02-03 13:36:59 +0200

    uploading pictures to the server(Multer)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 
 import mongoose from "mongoose";
 
@@ -22,11 +23,28 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads"); //file path
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname); //file name
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json()); //express shoud read json response
 
 app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+}); //single-middleware multer.
 
 app.get("/posts", PostController.getAll); //receiving all articles
 app.get("/posts/:id", PostController.getOne); //receiving one article
