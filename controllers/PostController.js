52209477ad0f11ebb62e0e907d3e291ff39b7bcc
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-02-11 21:52:08 +0200

    Conversions to tag array, moved to backend

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -112,7 +112,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(","),
       user: req.userId, //userId get when do an authorization check
     });
     const post = await doc.save();
