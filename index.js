dc393a6ebe987ae2bcdb18f009e8df18851e9e07
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-02-14 22:34:55 +0200

    vercel-3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { handleValidationErrors, checkAuth } from "./utils/index.js";
 import { UserController, PostController } from "./controllers/index.js";
 
 mongoose
-  .connect("process.env.MONGODB_URI")
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
 
